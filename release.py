7475a754897e6aa5ee5f676aba804b8e3511d116
dkaloc <dev@dusankaloc.net>
2025-12-25 18:51:09 +0100

    Fix sitemap

--- release.py
+++ release.py
@@ -382,17 +382,17 @@ def generate_sitemap():
       xml_lines.append(f'    <lastmod>{page.lastmod}</lastmod>')
       xml_lines.append(f'    <changefreq>{page.changefreq}</changefreq>')
       xml_lines.append(f'    <priority>{page.priority}</priority>')
-      xml_lines.append(f'    <xhtml:link rel="canonical" href="{escape_xml(page.loc)}" />')
+      xml_lines.append(f'    <xhtml:link rel="canonical" href="{escape_xml(page.loc).replace('/index.html', '').replace('.html', '')}" />')
 
       # Alternates (Hreflang)
       for alt_page in records:
         xml_lines.append(f'    <xhtml:link rel="alternate" hreflang="{alt_page.lang}" href="{escape_xml(alt_page.loc)}" />')
-        xml_lines.append(f'    <xhtml:link rel="alternate" hreflang="{alt_page.lang}" href="{escape_xml(alt_page.loc.replace('.html', ''))}" />')
+        xml_lines.append(f'    <xhtml:link rel="alternate" hreflang="{alt_page.lang}" href="{escape_xml(alt_page.loc.replace('/index.html', '').replace('.html', ''))}" />')
 
       # X-Default
       x_default = next((p for p in records if p.lang == 'cs'), records[0])
       xml_lines.append(f'    <xhtml:link rel="alternate" hreflang="x-default" href="{escape_xml(x_default.loc)}" />')
-      xml_lines.append(f'    <xhtml:link rel="alternate" hreflang="x-default" href="{escape_xml(x_default.loc.replace('.html', ''))}" />')
+      xml_lines.append(f'    <xhtml:link rel="alternate" hreflang="x-default" href="{escape_xml(x_default.loc.replace('/index.html', '').replace('.html', ''))}" />')
 
       # Images
       for img in page.images:
